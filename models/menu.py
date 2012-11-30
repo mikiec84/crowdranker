2eb52bead367eea01ee95f95967470a6f1d2df55
Luca de Alfaro <luca@dealfaro.com>
2012-11-30 15:10:38 -0800

    Initial commit.

---
+++ models/menu.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## Customize your APP title, subtitle and menus here
+#########################################################################
+
+response.title = T('CrowdLab Submission Ranking System')
+response.subtitle = T('')
+
+## read more at http://dev.w3.org/html5/markup/meta.name.html
+response.meta.author = 'Luca de Alfaro <luca@ucsc.edu>'
+response.meta.description = 'CrowdLab Submission Ranking System'
+response.meta.keywords = 'web2py, python, framework'
+response.meta.generator = 'Web2py Web Framework'
+
+## your http://google.com/analytics id
+response.google_analytics_id = None
+
+#########################################################################
+## this is the main application menu add/remove items as required
+#########################################################################
+
+response.menu = [
+    (T('Home'), False, URL('default', 'index'), [])
+]
+
+#########################################################################
+## provide shortcuts for development. remove in production
+#########################################################################
+
+
+def _():
+    # shortcuts
+    app = request.application
+    ctr = request.controller
+    # useful links to internal and external resources
+
+_()
