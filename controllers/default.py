edadd6f59c4d5a2570d72124ceaa8c8d8795806b
Luca de Alfaro <luca@dealfaro.com>
2012-12-04 23:48:04 -0800

    Checkpoint.

--- controllers/default.py
+++ controllers/default.py
@@ -52,8 +52,10 @@ def contest_managed_index():
     q = (db.contest.id.belongs(props.contest_can_manage))    
     # Keeps track of old managers, if this is an update.
     if len(request.args) > 2 and request.args[-3] == 'edit':
-        id = request.args[-1]
-        old_managers = db.contests[id].managers
+        c = db.contest[request.args[-1]]
+        old_managers = c.managers
+        old_submit_constraint = c.submit_constraint
+        old_rate_constraint = c.rate_constraint
     else:
         old_managers = []
     grid = SQLFORM.grid(q,
@@ -63,6 +65,7 @@ def contest_managed_index():
         create=True,
         onvalidate=validate_contest,
         oncreate=create_contest,
+        onupdate=update_contest(old_managers, old_submit_constrant, old_rate_constraint),
         )
     return dict(grid=grid)
     
@@ -72,22 +75,6 @@ def validate_contest(form):
     if auth.user.email not in form.vars.managers:
         form.vars.managers = [auth.user.email] + form.vars.managers
 
-def create_contest(form):
-    """Processes the creation of a context, propagating the effects."""
-    # First, we need to add the context for the new managers.
-    add_contest_to_user_managers(form.vars.id, form.vars.managers)
-    # If there is a submit constraint, we need to allow all the users
-    # in the list to submit.
-    if form.vars.submit_constraint != None:
-        user_list = db.user_list[form.vars.submit_constraint].email_list
-        # We need to add everybody in that list to submit.
-        add_contest_to_user_submit(form.vars.id, user_list)
-    # If there is a rating constraint, we need to allow all the users
-    # in the list to rate.
-    if form.vars.rate_constraint != None:
-        user_list = db.user_list[form.vars.rate_constraint].email_list
-        add_contest_to_user_rate(form.vars.id, user_list)
-                
 def add_contest_to_user_managers(id, user_list):
     for m in user_list:
         u = db(db.user_properties.email == m).select(db.user_properties.contests_can_manage).first()
@@ -118,7 +105,7 @@ def add_contest_to_user_submit(id, user_list):
         db(db.user_properties.email == m).update(contests_can_submit = l)
     db.commit()
         
-def add_contest_to_user_list(id, user_list):
+def add_contest_to_user_rate(id, user_list):
     for m in user_list:
         u = db(db.user_properties.email == m).select(db.user_properties.contests_can_rate).first()
         if u == None:
@@ -132,9 +119,76 @@ def add_contest_to_user_list(id, user_list):
         l = util.list_append_unique(l, id)
         db(db.user_properties.email == m).update(contests_can_rate = l)
     db.commit()
-        
 
-        
+def delete_contest_from_managers(id, managers):
+    for m in managers:
+        u = db(db.user_properties.email == m).select(db.user_properties.contests_can_manage).first()
+        if u != None:
+            l = util.list_remove(u.contests_can_manage, id)
+            db(db.user_properties.email == m).update(contests_can_manage = l)
+    db.commit()
+       
+def delete_contest_from_submitters(id, users):
+    for m in users:
+        u = db(db.user_properties.email == m).select(db.user_properties.contests_can_submit).first()
+        if u != None:
+            l = util.list_remove(u.contests_can_submit, id)
+            db(db.user_properties.email == m).update(contests_can_submit = l)
+    db.commit()
+       
+def delete_contest_from_raters(id, users):
+    for m in users:
+        u = db(db.user_properties.email == m).select(db.user_properties.contests_can_rate).first()
+        if u != None:
+            l = util.list_remove(u.contests_can_rate, id)
+            db(db.user_properties.email == m).update(contests_can_rate = l)
+    db.commit()
+                        
+def create_contest(form):
+    """Processes the creation of a context, propagating the effects."""
+    # First, we need to add the context for the new managers.
+    add_contest_to_user_managers(form.vars.id, form.vars.managers)
+    # If there is a submit constraint, we need to allow all the users
+    # in the list to submit.
+    if form.vars.submit_constraint != None:
+        user_list = db.user_list[form.vars.submit_constraint].email_list
+        # We need to add everybody in that list to submit.
+        add_contest_to_user_submit(form.vars.id, user_list)
+    # If there is a rating constraint, we need to allow all the users
+    # in the list to rate.
+    if form.vars.rate_constraint != None:
+        user_list = db.user_list[form.vars.rate_constraint].email_list
+        add_contest_to_user_rate(form.vars.id, user_list)
+                
+def update_contest(old_managers, old_submit_constraint, old_rate_constraint):
+    """A contest is being updated.  We need to return a callback for the form,
+    that will produce the proper update, taking into account the change in permissions."""
+    def f(form):
+        # Managers.
+        add_contest_to_user_managers(form.vars.id, util.list_diff(form.vars.managers, old_managers))
+        delete_contest_from_managers(form.vars.id, util.list_diff(old_managers, form.vars.managers))
+        # Submitters.
+        if old_submit_constraint != form.vars.submit_constraint:
+            # We need to update.
+            if old_submit_constraint != None:
+                user_list = db.user_list[old_submit_constraint]
+                delete_contest_from_submitters(form.vars.id, user_list)
+            if form.vars.submit_constraint != None:
+                user_list = db.user_list[form.vars.submit_constraint]
+                add_contest_to_user_submit(form.vars.id, user_list)
+        # Raters.
+        if old_rate_constraint != form.vars.rate_constraint:
+            # We need to update.
+            if old_rate_constraint != None:
+                user_list = db.user_list[old_rate_constraint]
+                delete_contest_from_raters(form.vars.id, user_list)
+            if form.vars.rate_constraint != None:
+                user_list = db.user_list[form.vars.rate_constraint]
+                add_contest_to_user_rate(form.vars.id, user_list)
+    return f
+                
+
+                
 def add_contest_to_managers(id, managers):
     for m in managers:
         u = db(db.user_properties.email == m).select(db.user_properties.user_lists).first()
