89858f3bb63cb187e0975b848dc5a4093079cbd9
Michael Shavlovsky <mshavlovsky@gmail.com>
2012-12-19 13:45:43 -0800

    ranker.py is added

--- modules/ranker.py
+++ modules/ranker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf8
 from gluon import *
+from rank import Rank
 
 def get_all_items_and_qdistr_param(db, contest_id):
     """ Returns a tuple (items, qdistr_param) where:
@@ -23,16 +24,36 @@ def get_all_items_and_qdistr_param(db, contest_id):
     # Ok, items and qdistr_param are filled.
     return items, qdistr_param
 
+def get_init_average_stdev():
+    """ Method returns tuple with average and stdev for initializin
+    field in table quality.
+    """
+    num_bins = 2001
+    avrg = num_bins / 2
+    stdev = num_bins / 8
+    return avrg , stdev
+
+def init_qdistr(db, contest_id, dist_type='gauss'):
+    """ Initializing average and stdev values in table quality for each
+    submission in given contest.
+    """
+    if dist_type == 'gauss':
+        num_bins = 2001
+        avrg = num_bins / 2
+        stdev = num_bins / 8
+        db(db.quality.contest_id == contest_id).update(average=avrg, stdev=stdev)
+    else:
+        raise Exception("Type of init distribution is not known.")
+
+
 def get_item(db, contest_id, user_id, old_items):
     """
-    If a user did not have items to rank then old_items is None.
+    If a user did not have items to rank then old_items is None. In this case
+    function returns two items to compare.
     """
     items, qdistr_param = get_all_items_and_qdistr_param(db, contest_id)
-    # todo(mshavlov): implement the rest
-
-    # As a mock, just returns the first submission.
-    s = db(db.submission.contest_id == contest_id).select(db.submission.id).first()
-    return s.id
+    rankobj = Rank.from_qdistr_param(items, qdistr_param)
+    return rankobj.sample_item(old_items)
 
 def process_comparison(db, contest_id, user_id, sorted_items, new_item):
     """ Function updates quality distributions and rank of submissions (items).
@@ -45,8 +66,15 @@ def process_comparison(db, contest_id, user_id, sorted_items, new_item):
         to the user. If sorted_items contains only two elements then
         new_item is None.
     """
+    # todo(michael): discuss concurrency issue
+    # as an example (db(query).select(..., for_update=True))
     items, qdistr_param = get_all_items_and_qdistr_param(db, contest_id)
-
-    # todo(mshavlov): Pass qdistr_param and items for updating quaity distributions and ranks.
-
-    # todo(mshavlov): Update quality distributions and ranks to the DB.
+    rankobj = Rank.from_qdistr_param(items, qdistr_param)
+    result = rankobj.update(sorted_items, new_item)
+    # Updating the DB.
+    for x in items:
+        perc, avrg, stdev = result[x]
+        db((db.quality.contest_id == contest_id) &
+           (db.quality.submission_id == x)).update(average=avrg,
+                                                     stdev=stdev,
+                                                percentile=perc)
