c32ce07876830bc522d5fdef8da0fd59ac1066bb
Luca de Alfaro <luca@dealfaro.org>
2012-12-21 20:53:17 -0800

    Implemented access checking for downloads.

--- controllers/submission.py
+++ controllers/submission.py
@@ -71,10 +71,11 @@ def submit():
     db.submission.title.readable = db.submission.title.writable = True
     # Produces an identifier for the submission.
     random_id = util.get_random_id()
-    form = SQLFORM(db.submission, sub, deletable=True, upload=URL('download'))
+    # TODO(luca): check that it is fine to do the download link without parameters.
+    form = SQLFORM(db.submission, sub, deletable=True, upload=URL('download_auhor', args=[None]))
     form.vars.contest_id = c.id
     form.vars.identifier = random_id
-    if request.vars.content != None:
+    if request.vars.content != None and request.vars.content != '':
         form.vars.original_filename = request.vars.content.filename
     # TODO(luca): once on appengine, see http://stackoverflow.com/questions/8008213/web2py-upload-with-original-filename
     # for changing the name of the file to the random_id.
@@ -93,10 +94,11 @@ def submit():
 @auth.requires_login()
 def resubmit():
     """Resubmit a particular submission, if the deadline is still open."""
-    sub = db.submission(request.args(0)) or redirect(URL('default', 'index'))
-    c = db.contest(sub.contest_id) or redirect(URL('default', 'index'))
-    if (sub.author != auth.user_id):
-        redirect(URL('default', 'index'))
+    subm = db.submission(request.args(0)) or redirect(URL('default', 'index'))
+    c = db.contest(subm.contest_id) or redirect(URL('default', 'index'))
+    if (subm.author != auth.user_id):
+        session.flash = T('You are not the author of the original submission.')
+        redirect(URL('feedback', 'index', args=['all']))
     # Checks if the submission deadline has passed.
     t = datetime.utcnow()
     if not (c.is_active and c.open_date <= t and c.close_date >= t):
@@ -105,9 +107,9 @@ def resubmit():
         redirect(URL('feedback', 'index', args=[c.id]))
     # The author can access the title.
     db.submission.title.readable = db.submission.title.writable = True
-    form = SQLFORM(db.submission, sub, deletable=True, upload=URL('download'))
-    form.vars.contest_id = sub.contest_id
-    if request.vars.content != None:
+    form = SQLFORM(db.submission, subm, deletable=True, upload=URL('download_author', args=[subm.id]))
+    form.vars.contest_id = subm.contest_id
+    if request.vars.content != None and request.vars.content != '':
         form.vars.original_filename = request.vars.content.filename
     if form.process().accepted:
         session.flash = T('Your resubmission has been accepted.')
@@ -123,16 +125,21 @@ def view_submission():
     # TODO(mbrich): here we need to be able to download the submission, and:
     # * For the author, download it under the original name.
     # * For a user, download it as t.submission_name + the original extension of the file.
-    t = db.task(request.args(0)) or redirect(URL('default', 'index'))
-    if t.user_id != auth.user_id:
+    v = validate_task(request.args(0), auth.user_id)
+    if v == None:
         session.flash(T('Not authorized.'))
         redirect(URL('default', 'index'))
-    subm = db.submission(t.submission_id) or redirect(URL('default', 'index'))
+    (t, subm, cont) = v
     # Shows the submission, except for the title (unless we are showing it to the author).
     if subm.author != auth.user_id:
         db.submission.title.readable = False
+    # We want to rename the submission content, so let us not display it as part of the form.
+    db.submission.content.readable = False
     form = SQLFORM(db.submission, record = subm, readonly=True)
-    return dict(form=form)
+    download_link = None
+    if subm.content != None and len(subm.content) > 0:
+        download_link = URL('download_reviewer', args=[t.id, subm.content])    
+    return dict(form=form, download_link=download_link)
 
    
 @auth.requires_login()
@@ -148,19 +155,57 @@ def view_own_submission():
     t = datetime.utcnow()
     download_link = None
     if (c.is_active and c.open_date <= t and c.close_date >= t):
-        form = SQLFORM(db.submission, subm, upload=URL('download'))
-        if request.vars.content != None:
+        form = SQLFORM(db.submission, subm, upload=URL('download_author', args=[subm.id]))
+        if request.vars.content != None and request.vars.content != '':
             form.vars.original_filename = request.vars.content.filename
         if form.process().accepted:
             session.flash = T('Your submission has been updated.')
             redirect(URL('feedback', 'index', args=['all']))
     else:
         db.submission.content.readable = False
-        form = SQLFORM(db.submission, subm, readonly=True, upload=URL('download'), buttons=[])
+        form = SQLFORM(db.submission, subm, readonly=True, upload=URL('download_author', args=[subm.id]), buttons=[])
         if subm.content != None and len(subm.content) > 0:
-            download_link = URL('download', args=[subm.content])
+            download_link = URL('download', args=[subm.content], vars=dict(s=subm.id))
     return dict(form=form, subm=subm, download_link=download_link)
 
-   
-def download():
+
+def validate_task(t_id, user_id):
+    """Validates that user_id can do the reviewing task t."""
+    t = db.task(request.args(0))
+    if t == None:
+        return None
+    if t.user_id != auth.user_id:
+        return None
+    subm = db.submission(t.submission_id)
+    if subm == None:
+        return None
+    c = db.contest(subm.contest_id)
+    if c == None:
+        return None
+    t = datetime.utcnow()
+    if c.rate_open_date > t or c.rate_close_date < t:
+        return None
+    return (t, s, c)
+
+
+@auth.requires_login()
+def download_author():
+    # The user must be the owner of the submission.
+    subm = db.submission(request.args(0))
+    if subm.author != auth.user_id:
+        session.flash = T('Not authorized.')
+        redirect(URL('default', 'index'))
+    request.args = request.args[1:]
+    return response.download(request, db)
+    
+@auth.requires_login()
+def download_reviewer():
+    # Checks that the reviewer has access.
+    v = validate_task(request.args(0), auth.user_id)
+    if v == None:
+        session.flash = T('Not authorized.')
+        redirect(URL('default', 'index'))
+    (t, s, c) = v
+    # TODO(luca): rename download.
+    request.args = request.args[1:]    
     return response.download(request, db)
