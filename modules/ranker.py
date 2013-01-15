25468fce38a8c8a3f5d20af6f3d18b236afc1768
Michael Shavlovsky <mshavlovsky@gmail.com>
2013-01-14 22:17:24 -0800

    can user rank own submissions is made as param

--- modules/ranker.py
+++ modules/ranker.py
@@ -30,13 +30,27 @@ def get_all_items_and_qdistr_param(db, venue_id):
     # Ok, items and qdistr_param are filled.
     return items, qdistr_param
 
+def get_qdistr_param(db, venue_id, items_id):
+    if items_id == None:
+        return None
+    qdistr_param = []
+    for x in items_id:
+        quality = db((db.quality.venue_id == venue_id) &
+                  (db.quality.submission_id == x)).select(db.quality.average,
+                  db.quality.stdev).first()
+        if quality == None:
+            return None
+        qdistr_param.append(quality.average)
+        qdistr_param.append(quality.stdev)
+    return qdistr_param
+
 def get_init_average_stdev():
     """ Method returns tuple with average and stdev for initializing
     field in table quality.
     """
     return AVRG, STDEV
 
-def get_item(db, venue_id, user_id, old_items):
+def get_item(db, venue_id, user_id, old_items, can_rank_own_submissions=False):
     """
     If a user did not have items to rank then old_items is None or empty string.
     """
@@ -44,14 +58,15 @@ def get_item(db, venue_id, user_id, old_items):
     # If items is None then some submission does not have qualities yet,
     # we need to know qualities of for all submission to correctly choose an
     # item.
-    if items == None:
+    if items == None or len(items) == 0:
         return None
     rankobj = Rank.from_qdistr_param(items, qdistr_param)
-    # Find submission that is authored by the user.
-    users_submission_id = db((db.submission.venue_id == venue_id) &
-                            (db.submission.author == user_id)).select(db.submission.id).first()
-    # TODO(michael): make nest line smarter using db.venue.can_run_own_submis
-    users_submission_id = None
+    if not can_rank_own_submissions:
+        # Find submission that is authored by the user.
+        users_submission_id = db((db.submission.venue_id == venue_id) &
+                                (db.submission.author == user_id)).select(db.submission.id).first().id
+    else:
+        users_submission_id = None
     return rankobj.sample_item(old_items, users_submission_id)
 
 def process_comparison(db, venue_id, user_id, sorted_items, new_item):
@@ -65,17 +80,17 @@ def process_comparison(db, venue_id, user_id, sorted_items, new_item):
         to the user. If sorted_items contains only two elements then
         new_item is None.
     """
-    if len(sorted_items) <= 1:
+    if sorted_items == None or len(sorted_items) <= 1:
         return None
-    items, qdistr_param = get_all_items_and_qdistr_param(db, venue_id)
-    # If items is None then some submission does not have qualities yet,
+    qdistr_param = get_qdistr_param(db, venue_id, sorted_items)
+    # If qdistr_param is None then some submission does not have qualities yet,
     # therefore we cannot process comparison.
-    if items == None:
+    if qdistr_param == None:
         return None
-    rankobj = Rank.from_qdistr_param(items, qdistr_param)
+    rankobj = Rank.from_qdistr_param(sorted_items, qdistr_param)
     result = rankobj.update(sorted_items, new_item)
     # Updating the DB.
-    for x in items:
+    for x in sorted_items:
         perc, avrg, stdev = result[x]
         db((db.quality.venue_id == venue_id) &
            (db.quality.submission_id == x)).update(average=avrg, stdev=stdev, percentile=perc)
